f44f806d8128d4fcb3b6adfdcd9502724f7d2f9c
Camilo Jaramillo <camiloj@mill5.com>
2021-03-12 11:13:15 -0500

    Initial logic to get the opening figi api information

---
+++ m5finance.tests/OpenFigiTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M5Finance.Tests
+{
+    [TestClass]
+    public class OpenFigiTests
+    {
+        private readonly OPENFIGIClient _client;
+
+        public OpenFigiTests()
+        {
+            _client = new OPENFIGIClient();
+        }
+
+        [TestMethod]
+        public async Task GetOpenFigiSecuritiesTestAsync()
+        {
+            var list = new List<OpenFIGIRequest>()
+                {
+                    new OpenFIGIRequest("TICKER", "MSFT"),
+                };
+
+            var figiInstrumentList = await _client.GetFigiMappingsAsync(new List<OpenFIGIRequest>());
+
+            Assert.IsNotNull(figiInstrumentList);
+            Assert.IsTrue(figiInstrumentList.Count() == 40);
+        }
+    }
+}
---
+++ m5finance/Clients/OPENFIGI/OPENFIGIClient.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using static Pineapple.Common.Preconditions;
+using static System.Environment;
+
+namespace M5Finance
+{
+    public class OPENFIGIClient 
+    {
+        private const string OPENFIGI_SECURITIES_URL = "https://api.openfigi.com/v1/mapping";
+
+        private readonly HttpClient _client;
+
+        public OPENFIGIClient()
+        {
+            _client = HttpInternal.Client;
+        }
+
+        public OPENFIGIClient(HttpClient client)
+        {
+            CheckIsNotNull(nameof(client), client);
+            _client = client;
+        }
+
+        /// <summary>
+        /// Brings the OpenFigi data for mapping security identifiers
+        /// </summary>
+        /// <param name="openFigiRequestList">List of parameters</param>
+        /// <returns>List of opening figi instruments.</returns>
+        public async Task<IEnumerable<OpenFIGIInstrument>> GetFigiMappingsAsync(IEnumerable<OpenFIGIRequest> openFigiRequestList)
+        {
+            CheckIsNotNull(nameof(openFigiRequestList), openFigiRequestList);
+            CheckIsWellFormedUri(nameof(OPENFIGI_SECURITIES_URL), OPENFIGI_SECURITIES_URL, UriKind.Absolute);
+            
+            _client.DefaultRequestHeaders.Add("X-OPENFIGI-APIKEY", Environment.GetEnvironmentVariable("OpenFigiApiKey"));
+
+            var response = await _client.SendAsStringAsync(OPENFIGI_SECURITIES_URL, JsonConvert.SerializeObject(openFigiRequestList, Formatting.Indented, new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            }));
+
+            var result = JsonConvert.DeserializeObject<List<OpenFIGIArrayResponse>>(response);
+
+            if (result.First().Error == null || !result.First().Error.Any())
+            {
+                // ToDo: Pending to cover more than one data array
+                return result.First().Data;
+            }
+            else
+            {
+                throw new Exception(result.First().Error);
+            }
+        }
+    }
+}
