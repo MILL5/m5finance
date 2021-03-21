d71203d83168102638ef1c927a16136fcf8ee3a0
Richard Crane <rich@mill5.com>
2021-03-21 08:14:11 -0400

    yahoo

---
+++ m5finance/Providers/Yahoo/Security/YahooSecurityService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M5Finance
+{
+    public class YahooSecurityService : ISecurityService
+    {
+        private const string ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+        // https://query1.finance.yahoo.com/v7/finance/lookup?formatted=true&lang=en-US&region=US&query={LETTER}*&type=equity&count10000&start=0
+
+        //public Task<IEnumerable<string>> GetExchangeMicCodesAsync()
+        //{
+        //}
+
+        //public Task<IEnumerable<Security>> GetSecuritiesAsync()
+        //{
+        //}
+
+        //public Task<Security> GetSecuritiesAsync(string ticker, string exchange)
+        //{
+        //}
+
+        //public Task<IEnumerable<Security>> GetSecuritiesByExchangeAsync(string exchange)
+        //{
+        //}
+
+        //public Task<IEnumerable<Security>> GetSecuritiesByTickerAsync(string ticker)
+        //{
+        //}
+
+        //public Task<IEnumerable<string>> GetTickersAsync()
+        //{
+        //}
+    }
+}
